e3e9f4a32081d3471c707585bfdc77fe2c366f19
lapine333 <87116686+lapine333@users.noreply.github.com>
2024-04-09 16:33:18 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,77 @@
+
+var arr;
+
 document.addEventListener('DOMContentLoaded', function() {
     
     // ui 설정
     uiSet();
 
+    // 2차 배열 생성 9 x 9. 총 81개 필요
+    arr = create2DArray(9, 9);
+
+    console.log("arr = ",arr);
+
+    // 배열에 랜덤한 숫자 넣기.
+    var v1;
+    var v2;
+    var v3;
+    var v4; 
+
+    v1 = random(1,9);
+    arr[0][0] = v1; 
+
+    while (1) {
+      var v2 = random(1,9);
+      if(v1 != v2) {
+         arr[0][1] = v2; 
+        break;
+      }
+    }
+
+    while (1) {
+      var v3 = random(1,9);
+      if(v1 != v2 
+        && v1 != v3)
+        break;
+    }
+
+    while (1) {
+      var v4 = random(1,9);
+      if(v1 != v2 
+        && v1 != v3
+        && v1 != v4)
+        break;
+    }
+
+    console.log("v1 = ",v1);
+    console.log("v2 = ",v2);
+    console.log("v3 = ",v3);
+
+    console.log("arr = ",arr);
   });
+  
+// 2 차 배열 생성
+function create2DArray(rows, columns) {
+    var arr = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        arr[i] = new Array(columns);
+    }
+    return arr;
+}
+
+// 최소값부터 최대값까지의 정수를 랜덤하게 생성.
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// 어레이에 속해있는지 체크
+function isContain(array) {
+  
+}
+
+
+// 4   3   5
+//
+// 2   4   3
+//
+// 1   3   2
