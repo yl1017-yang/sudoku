13d3a8c3ae2eaa88f5760815fcc71374b3ca72dd
lapine333 <87116686+lapine333@users.noreply.github.com>
2024-04-12 16:03:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,30 @@
 
-var total_array;
+//var total_array;
 
 document.addEventListener('DOMContentLoaded', function() {
     
     // ui 설정
     uiSet();
 
-    for(var n=0;n<10000;n++) {
-      if(setRandomArray()){
-        console.log("success count = ",n);
-        break;
-      }
-    }
-  });
+    // 랜덤 어레이를 생성한다.
+    setRandomArray();
+});
 
 function setRandomArray() {
+
+  var array = create2DArray(9,9); 
+  for(var n=0;n<1000;n++) {
+    if(fnSetRandomArray(array)){
+      console.log("success count = ",n);
+      break;
+    }
+  }
+
+  console.log("array = ",array);
+
+}
+
+function fnSetRandomArray(array) {
 
     // 추후 개선이 필요하다.***
     // 하나씩 만들어본다.
@@ -23,7 +33,8 @@ function setRandomArray() {
     // 2. 줄에서 공통된게 있는지
     // 3. 박스 (9개) 에서 공통된게 있는지
 
-    total_array = create2DArray(9,9); 
+    // 어레이 초기화
+    initArray(array,9,9,-1);
 
     var box_s_row = 0;
     var box_e_row = 2;
@@ -34,7 +45,7 @@ function setRandomArray() {
       var box_e_col = 2;
 
       for(var i=0;i<3;i++) {
-        if(!setBoxArray(box_s_row,box_e_row,box_s_col,box_e_col))
+        if(!setBoxArray(array,box_s_row,box_e_row,box_s_col,box_e_col))
           return false;
 
         box_s_col += 3;
@@ -45,19 +56,18 @@ function setRandomArray() {
       box_e_row += 3;
     }
     
-    console.log("total_array = ",total_array);
-
     return true;
 }  
 
-function setBoxArray(box_s_row,
-                     box_e_row,
-                     box_s_col,
-                     box_e_col) {
+function setBoxArray(array
+                     ,box_s_row
+                     ,box_e_row
+                     ,box_s_col
+                     ,box_e_col) {
 
     for(var n=box_s_row;n<=box_e_row;n++) {
       for(var i=box_s_col;i<=box_e_col;i++) {
-        if(!setArray(total_array
+        if(!setArray(array
           ,n
           ,i
           ,box_s_row
@@ -67,7 +77,17 @@ function setBoxArray(box_s_row,
           return false;
       }
     }
+    //console.log("array = ",array);
     return true;
+}
+
+// 어레이 초기화
+function initArray(array,row,col,initValue) {
+  for (var n=0;n<9;n++) {
+    for (var i=0;i<9;i++) {
+      array[n][i] = -1;
+    }  
+  }
 }
 
 function setArray(array
@@ -83,21 +103,23 @@ function setArray(array
       for(var n=0;n<random_array1.length;n++) {
         var v= random_array1[n];
 
+        /*
         var b1 = isContainRow(array,row,v);
         var b2 = isContainCol(array,col,v);
         var b3 = isContainBox(array
-          ,box_s_row
-          ,box_e_row
-          ,box_s_col
-          ,box_e_col
-          ,v );
+                              ,box_s_row
+                              ,box_e_row
+                              ,box_s_col
+                              ,box_e_col
+                              ,v );
 
-        //console.log("----");  
-        //console.log("v = " + v + " ,b1 = ",b1);
-        //console.log("v = " + v + " ,b2 = ",b2);
-        //console.log("v = " + v + " ,b3 = ",b3);
-        //console.log("----");
-  
+        console.log("----");  
+        console.log("v = " + v + " ,b1 = ",b1);
+        console.log("v = " + v + " ,b2 = ",b2);
+        console.log("v = " + v + " ,b3 = ",b3);
+        console.log("----");
+        */
+
         // 열, 박스 체크
         if(!isContainRow(array,row,v)
           &&!isContainCol(array,col,v)
@@ -107,7 +129,14 @@ function setArray(array
                           ,box_s_col
                           ,box_e_col
                           ,v )) {
-          array[row][col] = v;
+          var obj = new Object();
+          //obj.num = v;  // 숫자
+          //obj.input = -1; // 입렵값
+
+          //var ran = random(1,3);
+          //obj.isVisible = ran != 1 ? true : false; // 보이는 여부
+
+          array[row][col] = v; //obj;  // =v;
           return true;
           //break;
         }
@@ -163,7 +192,7 @@ function isContain(array,v) {
 function isContainRow(array,row,v) {
 
   for(var n=0;n<9;n++) {
-    var row_v= total_array[row][n];
+    var row_v= array[row][n];
 
     if( v == row_v ) {
       return true;
@@ -176,7 +205,7 @@ function isContainRow(array,row,v) {
 function isContainCol(array,col,v) {
 
   for(var n=0;n<9;n++) {
-    var col_v= total_array[n][col];
+    var col_v= array[n][col];
 
     if( v == col_v ) {
       return true;
