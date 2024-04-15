fadaeee0a9bfa8584e80f050d48237a32093d3c8
lapine333 <87116686+lapine333@users.noreply.github.com>
2024-04-16 05:56:07 +0900

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,33 +1,45 @@
 
 var uiSet = function() {
-    let lastFocused;
-    const inputs = document.querySelectorAll('.cell input');
+  let lastFocused;
+  const inputs = document.querySelectorAll('.cell input');
 
-    inputs.forEach(input => {
-      input.addEventListener('focus', function() {
-        
-        const row = this.getAttribute('data-row');
-        const col = this.getAttribute('data-col');
+  inputs.forEach(input => {
+    input.addEventListener('focus', function() {
+      
+      const row = this.getAttribute('data-row');
+      const col = this.getAttribute('data-col');
 
-        document.querySelectorAll('.cell input').forEach(input => {
-            input.classList.remove('highlight');
-        });
-
-        document.querySelectorAll(`.cell input[data-row="${row}"]`).forEach(input => {
-            input.classList.add('highlight');
-        });
-
-        document.querySelectorAll(`.cell input[data-col="${col}"]`).forEach(input => {
-            input.classList.add('highlight');
-        });
-
-        lastFocused = this;
+      document.querySelectorAll('.cell input').forEach(input => {
+          input.classList.remove('highlight');
       });
-    });
 
-    window.insertNumber = function(number) {
-      if (lastFocused && !lastFocused.readOnly) {
-          lastFocused.value = number;
-      }
-    } 
+      document.querySelectorAll(`.cell input[data-row="${row}"]`).forEach(input => {
+          input.classList.add('highlight');
+      });
+
+      document.querySelectorAll(`.cell input[data-col="${col}"]`).forEach(input => {
+          input.classList.add('highlight');
+      });
+
+      lastFocused = this;
+    });
+  });
+
+  window.insertNumber = function(number) {
+    if (lastFocused && !lastFocused.readOnly) {
+
+        console.log("lastFocused=",lastFocused);
+
+        var row = lastFocused.getAttribute('data-row');
+        var col = lastFocused.getAttribute('data-col');
+
+        lastFocused.value = number;
+
+        if(isValidNumber(row,col,number)) {
+          lastFocused.style.color = "#0000ff"; 
+        } else {
+          lastFocused.style.color = "#ff0000";   
+        }
+    }
+  } 
 }
