dbae8d82ae63a0f58d68c3adada6a6ea40975364
lapine333 <87116686+lapine333@users.noreply.github.com>
2024-04-24 20:43:24 +0900

    레벨 적용~

--- assets/js/array-util.js
+++ assets/js/array-util.js
@@ -2,142 +2,169 @@
 
 // 어레이 초기화
 function initArray(array,row,col,initValue) {
-    for (var n=0;n<row;n++) {
-      for (var i=0;i<col;i++) {
-        array[n][i] = -1;
-      }  
-    }
+  for (var n=0;n<row;n++) {
+    for (var i=0;i<col;i++) {
+      array[n][i] = -1;
+    }  
   }
+}
 
-  // 어레이에 있는 숫자랑 같은지 비교한다.
-  function isSameNumber(array,row,col,v){
+// 어레이에 있는 숫자랑 같은지 비교한다.
+function isSameNumber(array,row,col,v){
 
-    console.log("ValidNumber = ",array[row][col].value); 
-    console.log("v = ",v); 
+  console.log("ValidNumber = ",array[row][col].value); 
+  console.log("v = ",v); 
 
-    if(v == array[row][col].value) 
-      return true;
-    else
-      return false;
+  if(v == array[row][col].value) 
+    return true;
+  else
+    return false;
 
-  }
+}
 
 // 1부터 9까지 랜덤하게 숫자를 생성해 어레이에 넣는다. 각각은 고유하다.  
 function createRandomArray(size,min,max) {
-  
-    var random_array = new Array(size);
-  
-    // 1부터 9까지 숫자를 생성한다. 포함되지않게.
-    for(var n = 0;n< random_array.length;n++){
-      while (1) {
-        var v = random(min,max);
-        if(!this.isContain(random_array,v)) {
-          random_array[n] = v;
-          break;
-        }
-      }
-    }
-  
-    return random_array;
-  }    
 
-  // 2 차 배열 생성
- function create2DArray(rows, columns) {
-    var arr = new Array(rows);
-    for (var i = 0; i < rows; i++) {
-        arr[i] = new Array(columns);
+  var random_array = new Array(size);
+
+  // 1부터 9까지 숫자를 생성한다. 포함되지않게.
+  for(var n = 0;n< random_array.length;n++){
+    while (1) {
+      var v = random(min,max);
+      if(!this.isContain(random_array,v)) {
+        random_array[n] = v;
+        break;
+      }
     }
-    return arr;
   }
-  
-  // 최소값부터 최대값까지의 정수를 랜덤하게 생성.
-  function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return random_array;
+}    
+
+// 2 차 배열 생성
+function create2DArray(rows, columns) {
+  var arr = new Array(rows);
+  for (var i = 0; i < rows; i++) {
+      arr[i] = new Array(columns);
   }
-  
-  // 1차 어레이에 속해있는지 체크
-  function isContain(array,v) {
-    for (var i = 0; i < array.length; i++) {
-      if(v == array[i]) 
-        return true;
+  return arr;
+}
+
+// 최소값부터 최대값까지의 정수를 랜덤하게 생성.
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// 1차 어레이에 속해있는지 체크
+function isContain(array,v) {
+  for (var i = 0; i < array.length; i++) {
+    if(v == array[i]) 
+      return true;
+  }
+  return false
+}
+
+// 2차 어레이에서 줄에 같은값이 있는지 체크
+function isContainRow(array,row,v) {
+
+  for(var n=0;n<9;n++) {
+    var row_v= array[row][n];
+
+    if( v == row_v ) {
+      return true;
     }
-    return false
   }
-  
-  // 2차 어레이에서 줄에 같은값이 있는지 체크
-  function isContainRow(array,row,v) {
-  
-    for(var n=0;n<9;n++) {
-      var row_v= array[row][n];
-  
-      if( v == row_v ) {
+  return false;
+}
+
+// 2차 어레이에서 열에 같은 값이 있는지 체크
+function isContainCol(array,col,v) {
+
+  for(var n=0;n<9;n++) {
+    var col_v= array[n][col];
+
+    if( v == col_v ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// 박스에 같은 값이 있는지 체크
+function isContainBox(array,s_row,e_row,s_col,e_col,v) {
+  for(var n=s_row;n<=e_row;n++) {
+    for(var i=s_col;i<=e_col;i++) {
+      var box_v = array[n][i];
+      if( v == box_v ) {
         return true;
       }
     }
-    return false;
   }
-  
-  // 2차 어레이에서 열에 같은 값이 있는지 체크
-  function isContainCol(array,col,v) {
-  
-    for(var n=0;n<9;n++) {
-      var col_v= array[n][col];
-  
-      if( v == col_v ) {
-        return true;
-      }
+  return false;
+}
+
+// 2차 어레이에서 줄에 값이 모두 같은지 체크
+function isSameRow(array,row,v) {
+
+  for(var n=0;n<9;n++) {
+    var row_v= array[row][n];
+
+    if( v != row_v ) {
+      return false;
     }
-    return false;
   }
-  
-  // 박스에 같은 값이 있는지 체크
-  function isContainBox(array,s_row,e_row,s_col,e_col,v) {
-    for(var n=s_row;n<=e_row;n++) {
-      for(var i=s_col;i<=e_col;i++) {
-        var box_v = array[n][i];
-        if( v == box_v ) {
-          return true;
-        }
-      }
+  return true;
+}
+
+// 2차 어레이에서 열에 값이 모두 같은지 체크
+function isSameCol(array,col,v) {
+
+  for(var n=0;n<9;n++) {
+    var col_v= array[n][col];
+
+    if( v != col_v ) {
+      return false;
     }
-    return false;
   }
-  
-  // 2차 어레이에서 줄에 값이 모두 같은지 체크
-  function isSameRow(array,row,v) {
-  
-    for(var n=0;n<9;n++) {
-      var row_v= array[row][n];
-  
-      if( v != row_v ) {
+  return true;
+}
+
+// 박스에 값이 모두 같은지 체크
+function isSameBox(array,s_row,e_row,s_col,e_col,v) {
+  for(var n=s_row;n<=e_row;n++) {
+    for(var i=s_col;i<=e_col;i++) {
+      var box_v = array[n][i];
+      if( v != box_v ) {
         return false;
       }
     }
-    return true;
   }
-  
-  // 2차 어레이에서 열에 값이 모두 같은지 체크
-  function isSameCol(array,col,v) {
-  
-    for(var n=0;n<9;n++) {
-      var col_v= array[n][col];
-  
-      if( v != col_v ) {
-        return false;
+  return true;
+}
+
+// 값이 있는 총 횟수를 체크한다.
+function checkValueCount(array,s_row,e_row,s_col,e_col,v, min, max) {
+  //console.log("checkValueCount start");
+  //console.log("checkValueCount min = " + min);
+  //console.log("checkValueCount max = " + max);
+  var count = 0;
+  for(var n=s_row;n<e_row;n++) {
+    for(var i=s_col;i<e_col;i++) {
+      var array_v = array[n][i];
+      if(array_v == v) {
+        count++;
       }
     }
+  }
+
+  //console.log("checkValueCount count = ",count);
+
+  if(count >= min && count <= max) {
+    console.log("checkValueCount count = ",count);
     return true;
   }
-  
-  // 박스에 값이 모두 같은지 체크
-  function isSameBox(array,s_row,e_row,s_col,e_col,v) {
-    for(var n=s_row;n<=e_row;n++) {
-      for(var i=s_col;i<=e_col;i++) {
-        var box_v = array[n][i];
-        if( v != box_v ) {
-          return false;
-        }
-      }
-    }
-    return true;
+  else {
+    console.log("checkValueCount fail : count = ",count);
+    return false;
   }
+}
